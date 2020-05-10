1c312d58a28380bcca0b924aa917ac6e1f09b14e
Adam Voigt <kythex21@gmail.com>
2020-05-10 13:17:02 -0500

    changing title

--- DBM-CountPack-Lobos/CountLoadList.lua
+++ DBM-CountPack-Lobos/CountLoadList.lua
@@ -1,6 +1,6 @@
 local inserted = false
 function DBMCPLobos()
 	if inserted then return end
-	tinsert(DBM.Counts, {	text	= "Lobos",	value 	= "Lobos", path = "Interface\\AddOns\\DBMLOBOSCOUNTDOWN\\Lobos\\", max = 10})
+	tinsert(DBM.Counts, {	text	= "Lobos",	value 	= "Lobos", path = "Interface\\AddOns\\DBM-CountPack-Lobos\\Lobos\\", max = 10})
 	inserted = true
 end
